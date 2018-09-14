ee0a0f8b89ba47d5cfaa27de89adeb5904205b4d
lourod1987 <louart87@gmail.com>
2018-09-13 23:28:52 -0500

    creating a new component to add to render

--- src/App.js
+++ src/App.js
@@ -2,18 +2,30 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+class Contacts extends Component {
+    render() {
+        const contacts = [
+        {name: 'Roger'},
+        {name: 'Bill'},
+        {name: 'Jeff'},
+        {name: 'Will'},
+        {name: 'Kelly'}
+      ];
+
+      return <ol>
+          {contacts.map(contact => (
+              <li key={contact.name}>{contact.name}</li>
+          ))}
+      </ol>
+    }
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Contacts />
     );
   }
 }
