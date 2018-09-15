37e1752d9e89dd53f21469858f8b5cb4dd316ad9
lourod1987 <louart87@gmail.com>
2018-09-15 14:55:04 -0500

    new default state for Contacts project

--- src/App.js
+++ src/App.js
@@ -1,49 +1,9 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-
-
-class Contacts extends Component {
-    render() {
-        const contacts = this.props.people;
-
-      return <ol>
-          {contacts.map(contact => (
-              <li key={contact.name}>{contact.name}</li>
-          ))}
-      </ol>
-    }
-}
-
 class App extends Component {
-  render() {
-    return (
-        <div className="App">
-            <Contacts people={[
-                {name: 'Roger'},
-                {name: 'Bill'},
-                {name: 'Jeff'},
-                {name: 'Will'},
-                {name: 'Kelly'}
-            ]}/>
-            <Contacts people={[
-                {name: 'Nina'},
-                {name: 'Jill'},
-                {name: 'Lily'},
-                {name: 'Osiris'},
-                {name: 'Joe'}
-            ]}/>
-            <Contacts people={[
-                {name: 'Elle'},
-                {name: 'Nate'},
-                {name: 'Victoria'},
-                {name: 'Louis'},
-                {name: 'Madeline'}
-            ]}/>
+    render() {
+      return (
+        <div>
+          Hello World
         </div>
-    );
+      )
+    }
   }
-}
-
-export default App;
